1665d5b31657cf7291082a46ecbfb1a6b6b97f2b
Andrew Hoefling <andrewh@fileonq.com>
2021-07-25 00:34:48 -0400

    Removed stale benchmark test and updated

--- src/FileOnQ.Imaging.Raw.Benchmarking/Program.cs
+++ src/FileOnQ.Imaging.Raw.Benchmarking/Program.cs
@@ -14,78 +14,21 @@ namespace FileOnQ.Imaging.Raw.Benchmarking
 	[MemoryDiagnoser]
 	public class LibRaw
 	{
-		readonly string librawInput = @"D:\FileOnQ.Imaging.Raw\images\sample1.cr2";
-		
-		[Benchmark(Description = "Buffers.MemoryCopy")]
-		public byte[] LibRawThumbnail_MemoryCopy()
-		{
-			using (var rawImage = new RawImage(librawInput))
-			{
-				rawImage.UnpackThumbnail();
-				return rawImage.GetThumbnailAsByteArray(0);
-			}
-		}
-		
-		[Benchmark(Description = "Marshal.Copy")]
-		public byte[] LibRawThumbnail_MarshalCopy()
-		{
-			using (var rawImage = new RawImage(librawInput))
-			{
-				rawImage.UnpackThumbnail();
-				return rawImage.GetThumbnailAsByteArray(1);
-			}
-		}
-		
-		// [Benchmark(Description = "Unsafe.CopyBlock")]
-		// public byte[] LibRawThumbnail_CopyBlock()
-		// {
-		// 	using (var rawImage = new RawImage(librawInput))
-		// 	{
-		// 		rawImage.UnpackThumbnail();
-		// 		return rawImage.GetThumbnailAsByteArray(2);
-		// 	}
-		// }
-		//
-		// [Benchmark(Description = "Unsafe.CopyBlockUnaligned")]
-		// public byte[] LibRawThumbnail_CopyBlockUnaligned()
-		// {
-		// 	using (var rawImage = new RawImage(librawInput))
-		// 	{
-		// 		rawImage.UnpackThumbnail();
-		// 		return rawImage.GetThumbnailAsByteArray(3);
-		// 	}
-		// }
-		
-		[Benchmark(Description = "Pointer Address Assignment")]
-		public byte[] LibRawThumbnail_PointerMath()
-		{
-			using (var rawImage = new RawImage(librawInput))
-			{
-				rawImage.UnpackThumbnail();
-				return rawImage.GetThumbnailAsByteArray(4);
-			}
-		}
-		
-		[Benchmark(Description = "Span<T>")]
-		public void LibRawThumbnail_SpanOfT()
-		{
-			//using (var rawImage = new RawImage(librawInput))
-			//{
-			//	rawImage.UnpackThumbnail();
-			//	return rawImage.GetThumbnailAsSpan();
-			//}
+		readonly string librawInput = @"images\sample1.cr2";
 
-			using (var rawImage = new RawImage(librawInput))
+		[Benchmark()]
+		public Span<byte> Thumbnail_AsSpan()
+		{
+			using (var image = new RawImage(librawInput))
+			using (var thumbnail = image.UnpackThumbnail())
 			{
-				rawImage.WriteTiff(@"D:\FileOnQ.Imaging.Raw\images\sample1.tiff");
-				//rawImage.UnpackThumbnail();
-				//return rawImage.GetThumbnailAsSpan();
+				return thumbnail.AsProcessedImage().Buffer;
 			}
 		}
 	}
 	
 	class Program
 	{
-		static void Main(string[] args) => new LibRaw().LibRawThumbnail_SpanOfT(); //BenchmarkRunner.Run<LibRaw>();
+		static void Main(string[] args) => BenchmarkRunner.Run<LibRaw>();
 	}
 }
