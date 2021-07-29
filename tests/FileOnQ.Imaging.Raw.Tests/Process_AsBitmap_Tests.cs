78392b79dc23633e703bba79743c834e39f21976
Andrew Hoefling <andrewh@fileonq.com>
2021-07-29 00:05:26 -0400

    Updated AsBitmap tests to use SHA256 file hash

--- tests/FileOnQ.Imaging.Raw.Tests/Process_AsBitmap_Tests.cs
+++ tests/FileOnQ.Imaging.Raw.Tests/Process_AsBitmap_Tests.cs
@@ -1,38 +1,39 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Reflection;
+using FileOnQ.Imaging.Raw.Tests.Utilities;
 using NUnit.Framework;
 
 namespace FileOnQ.Imaging.Raw.Tests
 {
-	[TestFixture("Images\\sample1.cr2")]
-	[TestFixture("Images\\@signatureeditsco(1).dng")]
-	[TestFixture("Images\\@signatureeditsco.dng")]
-	[TestFixture("Images\\canon_eos_r_01.cr3")] // fails on the GPU
-	[TestFixture("Images\\Christian - .unique.depth.dng")]
-	[TestFixture("Images\\DSC_0118.nef")]
-	[TestFixture("Images\\DSC02783.ARW")]
-	[TestFixture("Images\\PANA2417.RW2")]
-	[TestFixture("Images\\PANA8392.RW2")]
-	[TestFixture("Images\\photo by @Dupe.png--@Emily.rosegold.arw")]
-	[TestFixture("Images\\signature edits APC_00171.dng")]
-	[TestFixture("Images\\signature edits free raws P1015526.dng")]
-	[TestFixture("Images\\signature edits free raws_DSC7082.NEF")]
-	[TestFixture("Images\\signatureeditsfreerawphoto.NEF")]
+	[TestFixture("Images\\@signatureeditsco(1).dng", "BBA0AA3A611CBFA1B114037957061CC58D3484DA5005700D19F79860F0AA7DFC")]
+	[TestFixture("Images\\@signatureeditsco.dng", "937B694DE013859228F4209ACC93DCED0F9206B560A4949CA53EC695E80F52B5")]
+	[TestFixture("Images\\canon_eos_r_01.cr3", "F9FD0284A4C833CCCA85E8B8C4DF187EF8F23EAD485B5CBBA52BAE873660776E")] // fails on the GPU
+	[TestFixture("Images\\Christian - .unique.depth.dng", "585DA19A374F82BEA1AB202AEA03BA0B384BBC30D75CB0422D14576221153076")]
+	[TestFixture("Images\\DSC_0118.nef", "845BBAC7E73070725BD2F7E616784DA0736F37900A834CF6979142D41C043B6D")]
+	[TestFixture("Images\\DSC02783.ARW", "AAB21936231BE11A9AC48FA9DE685317F3AB4D0F350D7879E72CA62DA6935D67")]
+	[TestFixture("Images\\PANA2417.RW2", "BBB4582591D9689F4F586C3E361EA43FC73B598F693F67E38A79B35473A339C5")]
+	[TestFixture("Images\\PANA8392.RW2", "92DAAAAB9761A789F38303D397BF6E588C0CF6F9B02B5BD22EAF2395BC5711F5")]
+	[TestFixture("Images\\photo by @Dupe.png--@Emily.rosegold.arw", "4914D2574E020E819C516CD96126DFCF5B6027D245D8CB4442D014239AFE5D8B")]
+	[TestFixture("Images\\sample1.cr2", "02717173DBF92480B9FEC2AC099DCFFED546C1F920B2EF388B99051C95AE198F")]
+	[TestFixture("Images\\signature edits APC_00171.dng", "F04BBB33B1F2D01115309DBCC6C951D25AF998EC749B6893D37B91352B20D71E")]
+	[TestFixture("Images\\signature edits free raws P1015526.dng", "E3FC226754E815C0475C279559AE23C9EBDC5C57C63F0E484090ED1E5776B305")]
+	[TestFixture("Images\\signature edits free raws_DSC7082.NEF", "983E7E0FD41E2980DCFAC3A3C71C3F6BE88788DA6A6B8450EFA7578BED82F088")]
+	[TestFixture("Images\\signatureeditsfreerawphoto.NEF", "ABCC78EA917D681A2E36A240B64E40AA0E8727EE02CA71D8A5AD45ABBB8F0A96")]
 	public class Process_AsBitmap_Tests
 	{
 		readonly string input;
 		readonly string output;
-		readonly string expectedOutput;
+		readonly string hash;
 
-		public Process_AsBitmap_Tests(string path)
+		public Process_AsBitmap_Tests(string path, string expectedHash)
 		{
+			hash = expectedHash;
+
 			var assemblyDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
 			input = Path.Combine(assemblyDirectory, path);
 
 			var filename = Path.GetFileNameWithoutExtension(input);
 			var directory = Path.GetDirectoryName(input) ?? string.Empty;
-			expectedOutput = Path.Combine(directory, $"{filename}.process.bmp");
 			output = Path.Combine(assemblyDirectory, $"{filename}.process.bmp");
 		}
 
@@ -50,36 +51,13 @@ namespace FileOnQ.Imaging.Raw.Tests
 			using (var raw = image.UnpackRaw())
 			{
 				raw.Process(new DcrawProcessor());
-
-				var bitmap = raw.AsBitmap();
-				bitmap.Save(output);
-				bitmap.Dispose();
+				using (var bitmap = raw.AsBitmap())
+					bitmap.Save(output);
 			}
 
-			var expectedBuffer = new Span<byte>(File.ReadAllBytes(expectedOutput));
-			var actualBuffer = new Span<byte>(File.ReadAllBytes(output));
-
-			Assert.IsTrue(actualBuffer.Length > 0);
-			Assert.AreEqual(expectedBuffer.Length, actualBuffer.Length);
-
-			bool isValid = true;
-			for (int index = 0; index < expectedBuffer.Length; index++)
-			{
-				if (expectedBuffer[index] != actualBuffer[index])
-				{
-					isValid = false;
-					break;
-				}
-			}
-
-			Assert.IsTrue(isValid, "Expected buffer does not match actual buffer, files are different");
+			AssertUtilities.IsHashEqual(hash, File.ReadAllBytes(output));
 		}
 
-		// NOTE - This is failing for canon_eos cr3 file
-		// We get an error about corrupt memory. I think pixel data isn't being built correctly
-		// I think the cr3 image is invalid as we can't write blank pixels. This tells me that
-		// something isn't adding up. The GDI+ save API must be trying to access something outside
-		// the bounds of allocated memory (array). Maybe the width or height is wrong or the size
 		[Test]
 		public void ProcessAsBitmap_Gpu_Test()
 		{
@@ -89,29 +67,11 @@ namespace FileOnQ.Imaging.Raw.Tests
 				raw.Process(new DcrawProcessor());
 
 				var processedImage = raw.AsProcessedImage();
-				//File.WriteAllBytes("test.ppm", processedImage.Buffer.ToArray());
-				var bitmap = raw.AsBitmap(1);
-				bitmap.Save(output);
-				bitmap.Dispose();
+				using (var bitmap = raw.AsBitmap(1))
+					bitmap.Save(output);
 			}
-
-			var expectedBuffer = new Span<byte>(File.ReadAllBytes(expectedOutput));
-			var actualBuffer = new Span<byte>(File.ReadAllBytes(output));
-
-			Assert.IsTrue(actualBuffer.Length > 0);
-			Assert.AreEqual(expectedBuffer.Length, actualBuffer.Length);
-
-			bool isValid = true;
-			for (int index = 0; index < expectedBuffer.Length; index++)
-			{
-				if (expectedBuffer[index] != actualBuffer[index])
-				{
-					isValid = false;
-					break;
-				}
-			}
-
-			Assert.IsTrue(isValid, "Expected buffer does not match actual buffer, files are different");
+			
+			AssertUtilities.IsHashEqual(hash, File.ReadAllBytes(output));
 		}
 
 		[Test]
@@ -123,28 +83,11 @@ namespace FileOnQ.Imaging.Raw.Tests
 				raw.Process(new DcrawProcessor());
 
 				var processedImage = raw.AsProcessedImage();
-				var bitmap = raw.AsBitmap(2);
-				bitmap.Save(output);
-				bitmap.Dispose();
+				using (var bitmap = raw.AsBitmap(2))
+					bitmap.Save(output);
 			}
 
-			var expectedBuffer = new Span<byte>(File.ReadAllBytes(expectedOutput));
-			var actualBuffer = new Span<byte>(File.ReadAllBytes(output));
-
-			Assert.IsTrue(actualBuffer.Length > 0);
-			Assert.AreEqual(expectedBuffer.Length, actualBuffer.Length);
-
-			bool isValid = true;
-			for (int index = 0; index < expectedBuffer.Length; index++)
-			{
-				if (expectedBuffer[index] != actualBuffer[index])
-				{
-					isValid = false;
-					break;
-				}
-			}
-
-			Assert.IsTrue(isValid, "Expected buffer does not match actual buffer, files are different");
+			AssertUtilities.IsHashEqual(hash, File.ReadAllBytes(output));
 		}
 	}
 }
--- tests/FileOnQ.Imaging.Raw.Tests/Thumbnail_AsBitmap_Tests.cs
+++ tests/FileOnQ.Imaging.Raw.Tests/Thumbnail_AsBitmap_Tests.cs
@@ -1,39 +1,40 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Reflection;
+using FileOnQ.Imaging.Raw.Tests.Utilities;
 using NUnit.Framework;
 
 namespace FileOnQ.Imaging.Raw.Tests
 {
-	[TestFixture("Images\\sample1.cr2")]
-	[TestFixture("Images\\@signatureeditsco(1).dng")]
-	[TestFixture("Images\\@signatureeditsco.dng")]
-	[TestFixture("Images\\canon_eos_r_01.cr3")]
-	[TestFixture("Images\\Christian - .unique.depth.dng", ImageFormat.Bitmap)]
-	[TestFixture("Images\\DSC_0118.nef")]
-	[TestFixture("Images\\DSC02783.ARW")]
-	[TestFixture("Images\\PANA2417.RW2")]
-	[TestFixture("Images\\PANA8392.RW2")]
-	[TestFixture("Images\\photo by @Dupe.png--@Emily.rosegold.arw")]
-	[TestFixture("Images\\signature edits APC_00171.dng")]
-	[TestFixture("Images\\signature edits free raws P1015526.dng")]
-	[TestFixture("Images\\signature edits free raws_DSC7082.NEF")]
-	[TestFixture("Images\\signatureeditsfreerawphoto.NEF")]
+	[TestFixture("Images\\@signatureeditsco(1).dng", "2EF59B98A5ED52162A70D7B6A85DA2DCF6E2E51CB946DE9011032A52D91FB2E4")]
+	[TestFixture("Images\\@signatureeditsco.dng", "5E103695025C40ED0F78F47F29EBF5333D5B0529FDA955EE21DAECA6E6F3579F")]
+	[TestFixture("Images\\canon_eos_r_01.cr3", "653786673184C67C128EFC89B67A6BB7FB19551583EB4176BC278A20489784B4")]
+	[TestFixture("Images\\Christian - .unique.depth.dng", ImageFormat.Bitmap, "66EC0E0ABC132E005B6A163A898999D084AAE7E69DF670D40FA1E326C4406788")]
+	[TestFixture("Images\\DSC_0118.nef", "3D41F9C3B240484BD380DA01428C0F293D0FC958C3D923B70DB1DF433F33DCD3")]
+	[TestFixture("Images\\DSC02783.ARW", "D069B8AA36AC684C1B0F4EF6CF5C8C1EF562C1083C00D59E87D6B5CABDD45F26")]
+	[TestFixture("Images\\PANA2417.RW2", "04796EB9BB9613D6F7701527D70D816F97E678615E90A75C91D18E9EB68250BB")]
+	[TestFixture("Images\\PANA8392.RW2", "38792A6EDCE16E0CD9098AC8B3249B3E7BBD069AC78A65B080C87078829C0580")]
+	[TestFixture("Images\\photo by @Dupe.png--@Emily.rosegold.arw", "D24EAF12AFE8D8DDD309428BEB43116F9A3C1353804BCE47EB0E130B3C6E2E65")]
+	[TestFixture("Images\\sample1.cr2", "0674ECFC552DC9E4365D496FD10508333EAF71269F5451FED42AF5E2678B1A30")]
+	[TestFixture("Images\\signature edits APC_00171.dng", "6FEE1556F783D971F4AB6A5145116E92585D68C60B6F019140DACA7538E83FC2")]
+	[TestFixture("Images\\signature edits free raws P1015526.dng", "DF7C22D13CBABA9EB6DBC07A2723A139A9690EBC0894AFD5B272C2CD876C60D2")]
+	[TestFixture("Images\\signature edits free raws_DSC7082.NEF", "FD8C804D24CF864895AC5C2A0B4A27D932FE067661CD8DD9E7D725FBE7D018F2")]
+	[TestFixture("Images\\signatureeditsfreerawphoto.NEF", "A69D1A858C2C058BEA73C04099989F7F7AAAEF8BE355BA334F464E625DF7553A")]
 	public class Thumbnail_AsBitmap_Tests
 	{
 		readonly string input;
 		readonly string output;
-		readonly string expectedThumbnail;
+		readonly string hash;
 
-		public Thumbnail_AsBitmap_Tests(string path) : this(path, ImageFormat.Jpeg) { }
-		public Thumbnail_AsBitmap_Tests(string path, ImageFormat imageFormat)
+		public Thumbnail_AsBitmap_Tests(string path, string expectedHash) : this(path, ImageFormat.Jpeg, expectedHash) { }
+		public Thumbnail_AsBitmap_Tests(string path, ImageFormat imageFormat, string expectedHash)
 		{
+			hash = expectedHash;
+
 			var assemblyDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
 			input = Path.Combine(assemblyDirectory, path);
 
 			var filename = Path.GetFileNameWithoutExtension(input);
 			var directory = Path.GetDirectoryName(input) ?? string.Empty;
-			expectedThumbnail = Path.Combine(directory, $"{filename}.thumb.bmp");
 			output = Path.Combine(directory, $"{filename}.thumb.bmp");
 		}
 
@@ -54,23 +55,7 @@ namespace FileOnQ.Imaging.Raw.Tests
 				bitmap.Save(output);
 			}
 
-			var expectedBuffer = new Span<byte>(File.ReadAllBytes(expectedThumbnail));
-			var actualBuffer = new Span<byte>(File.ReadAllBytes(output));
-
-			Assert.IsTrue(actualBuffer.Length > 0);
-			Assert.AreEqual(expectedBuffer.Length, actualBuffer.Length);
-
-			bool isValid = true;
-			for (int index = 0; index < expectedBuffer.Length; index++)
-			{
-				if (expectedBuffer[index] != actualBuffer[index])
-				{
-					isValid = false;
-					break;
-				}
-			}
-
-			Assert.IsTrue(isValid, "Expected buffer does not match actual buffer, files are different");
+			AssertUtilities.IsHashEqual(hash, File.ReadAllBytes(output));
 		}
 
 		[Test]
@@ -83,23 +68,7 @@ namespace FileOnQ.Imaging.Raw.Tests
 				bitmap.Save(output);
 			}
 
-			var expectedBuffer = new Span<byte>(File.ReadAllBytes(expectedThumbnail));
-			var actualBuffer = new Span<byte>(File.ReadAllBytes(output));
-
-			Assert.IsTrue(actualBuffer.Length > 0);
-			Assert.AreEqual(expectedBuffer.Length, actualBuffer.Length);
-
-			bool isValid = true;
-			for (int index = 0; index < expectedBuffer.Length; index++)
-			{
-				if (expectedBuffer[index] != actualBuffer[index])
-				{
-					isValid = false;
-					break;
-				}
-			}
-
-			Assert.IsTrue(isValid, "Expected buffer does not match actual buffer, files are different");
+			AssertUtilities.IsHashEqual(hash, File.ReadAllBytes(output));
 		}
 
 		[Test]
@@ -112,23 +81,7 @@ namespace FileOnQ.Imaging.Raw.Tests
 				bitmap.Save(output);
 			}
 
-			var expectedBuffer = new Span<byte>(File.ReadAllBytes(expectedThumbnail));
-			var actualBuffer = new Span<byte>(File.ReadAllBytes(output));
-
-			Assert.IsTrue(actualBuffer.Length > 0);
-			Assert.AreEqual(expectedBuffer.Length, actualBuffer.Length);
-
-			bool isValid = true;
-			for (int index = 0; index < expectedBuffer.Length; index++)
-			{
-				if (expectedBuffer[index] != actualBuffer[index])
-				{
-					isValid = false;
-					break;
-				}
-			}
-
-			Assert.IsTrue(isValid, "Expected buffer does not match actual buffer, files are different");
+			AssertUtilities.IsHashEqual(hash, File.ReadAllBytes(output));
 		}
 	}
 }
