d91d99b660a90a4d131109446e7f4fae94217bc0
Andrew Hoefling <andrewh@fileonq.com>
2021-07-29 17:09:53 -0400

    Updated benchmark to load test our raw GPU vs CPU algorithms for converting ppm to bmp

---
+++ tests/FileOnQ.Imaging.Raw.Benchmarking/AsBitmap.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+using BenchmarkDotNet.Jobs;
+
+namespace FileOnQ.Imaging.Raw.Benchmarking
+{
+	[SimpleJob(RuntimeMoniker.Net48, launchCount: 5, invocationCount: 10)]
+	[SimpleJob(RuntimeMoniker.Net50, launchCount: 5, invocationCount: 10)]
+	[NativeMemoryProfiler]
+	[MemoryDiagnoser]
+	public class AsBitmap
+	{
+		readonly byte[] buffer;
+		readonly int width;
+		readonly int height;
+		readonly int stride;
+		GCHandle handle;
+		IntPtr pointer;
+
+		public AsBitmap()
+		{
+			var assemblyDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
+			var filePath = Path.Combine(assemblyDirectory, "..\\..\\..\\..\\generated-data-file");
+			if (!File.Exists(filePath))
+				throw new FileNotFoundException(filePath);
+
+			var directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
+			buffer = File.ReadAllBytes(Path.Combine(directory, filePath));
+			
+			// These files are hard-coded for the generated image. If the generated image changes the benchmark will fail
+			width = 8688;
+			height = 5792;
+			stride = width * 3 + (width % 4);
+		}
+
+		[Benchmark(Baseline = true)]
+		public Size ConvertPpmToBmp_CpuPtr()
+		{
+			var size = Size.Empty;
+			using (var bitmap = AsBitmapCpuPtr())
+				size = bitmap.Size;
+
+			if (handle != default)
+				handle.Free();
+
+			return size;
+		}
+
+		[Benchmark]
+		public Size ConvertPpmToBmp_Gpu()
+		{
+			var size = Size.Empty;
+			using (var bitmap = AsBitmapGpu())
+				size = bitmap.Size;
+
+			if (pointer != IntPtr.Zero)
+				Cuda.FreeMemory(pointer);
+
+			return size;
+		}
+
+		Bitmap AsBitmapCpuPtr()
+		{
+			var offset = width % 4;
+			var strideWithoutOffset = width * 3;
+			var stride = strideWithoutOffset + offset;
+
+			var additionalBytes = offset * height; // We should subtract offset to ensure we remove trailing bytes // Additional - this matches the native bitmap result
+			var bitmapBuffer = new byte[buffer.Length + additionalBytes];
+
+			var bitmapPosition = 0;
+			for (int position = 0; position < buffer.Length; position += 3)
+			{
+				if (position > 0 && position % strideWithoutOffset == 0)
+				{
+					for (int j = 0; j < offset; j++)
+					{
+						bitmapBuffer[bitmapPosition] = 0;
+						bitmapPosition++;
+					}
+				}
+
+				bitmapBuffer[bitmapPosition + 2] = buffer[position];
+				bitmapBuffer[bitmapPosition + 1] = buffer[position + 1];
+				bitmapBuffer[bitmapPosition] = buffer[position + 2];
+
+				bitmapPosition += 3;
+			}
+
+			handle = GCHandle.Alloc(bitmapBuffer, GCHandleType.Pinned);
+			return new Bitmap(width, height,
+				stride,
+				System.Drawing.Imaging.PixelFormat.Format24bppRgb,
+				handle.AddrOfPinnedObject());
+		}
+
+		Bitmap AsBitmapGpu()
+		{
+			var handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+
+			var error = Cuda.Error.Success;
+			int length = 0;
+			pointer = Cuda.ProcessBitmap(handle.AddrOfPinnedObject(), buffer.Length, width, height, ref error);
+
+			var stride = width * 3 + (width % 4);
+			return new Bitmap(width, height,
+				stride,
+				System.Drawing.Imaging.PixelFormat.Format24bppRgb,
+				pointer);
+		}
+	}
+}
--- tests/FileOnQ.Imaging.Raw.Benchmarking/Program.cs
+++ tests/FileOnQ.Imaging.Raw.Benchmarking/Program.cs
@@ -1,81 +1,37 @@
 ﻿using System;
-using System.Drawing;
 using System.IO;
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Diagnostics.Windows.Configs;
-using BenchmarkDotNet.Engines;
-using BenchmarkDotNet.Jobs;
+using System.Reflection;
 using BenchmarkDotNet.Running;
 
 namespace FileOnQ.Imaging.Raw.Benchmarking
 {
-	// TODO - 7/25/2021 - @ahoefling - Add x86 vs x64 platform support - https://github.com/dotnet/BenchmarkDotNet/issues/873
-	[SimpleJob(RuntimeMoniker.Net48, launchCount: 2, invocationCount: 10)]
-	[SimpleJob(RuntimeMoniker.Net50, launchCount: 2, invocationCount: 10)]
-	[NativeMemoryProfiler]
-	[MemoryDiagnoser]
-	public class LibRaw
-	{
-		readonly string librawInputBitmap = @"images\sample1.cr2";
-		//readonly string librawInputBitmap = @"Images\\Christian - .unique.depth.dng";
-		//readonly string librawInputBitmap = "Images\\canon_eos_r_01.cr3";
-
-		//[Benchmark()]
-		//public Span<byte> Thumbnail_AsSpan()
-		//{
-		//	using (var image = new RawImage(librawInput))
-		//	using (var thumbnail = image.UnpackThumbnail())
-		//	{
-		//		return thumbnail.AsProcessedImage().Buffer;
-		//	}
-		//}
-
-		//[Benchmark(Description = "Unpack Thumbnail", Baseline = true)]
-		//public object UnpackThumbnail()
-		//{
-		//	using (var image = new RawImage(librawInputBitmap))
-		//		return image.UnpackThumbnail();
-		//}
-
-		[Benchmark(Baseline = true)]
-		public Size Bitmap()
-		{
-			using (var image = new RawImage(librawInputBitmap))
-			using (var raw = image.UnpackRaw())
-			{
-				raw.Process(new DcrawProcessor());
-				using (var bitmap = raw.AsBitmap())
-					return bitmap.Size;
-			}
-		}
-
-		[Benchmark(Description = "Bitmap - IntPtr")]
-		public Size Bitmap_IntPtr()
-		{
-			using (var image = new RawImage(librawInputBitmap))
-			using (var raw = image.UnpackRaw())
-			{
-				raw.Process(new DcrawProcessor());
-				using (var bitmap = raw.AsBitmap(2))
-					return bitmap.Size;
-			}
-		}
-
-		[Benchmark(Description = "Bitmap - GPU")]
-		public Size Bitmap_Gpu()
-		{
-			using (var image = new RawImage(librawInputBitmap))
-			using (var raw = image.UnpackRaw())
-			{
-				raw.Process(new DcrawProcessor());
-				using (var bitmap = raw.AsBitmap(1))
-					return bitmap.Size;
-			}
-		}
-	}
-	
 	class Program
 	{
-		static void Main(string[] args) => BenchmarkRunner.Run<LibRaw>();
+		static readonly string input = "Images/sample1.cr2";
+		static void Main(string[] args)
+		{
+			AsBitmapRunner();
+		}
+
+		static void AsBitmapRunner()
+		{
+			Console.WriteLine("AsBitmap benchmark starting");
+			Console.WriteLine("Generating raw ppm file to use in benchmark, this operation may take sometime . . .");
+
+			var assemblyDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
+			var filePath = Path.Combine(assemblyDirectory, "generated-data-file");
+
+			using (var image = new RawImage(input))
+			using (var raw = image.UnpackRaw())
+			{
+				raw.Process(new DcrawProcessor());
+				var processedImage = raw.AsProcessedImage();
+
+				File.WriteAllBytes(filePath, processedImage.Buffer.ToArray());
+			}
+
+			Console.WriteLine("Input data generated, starting benchmark!");
+			BenchmarkRunner.Run<AsBitmap>();
+		}
 	}
 }
