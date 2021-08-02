112410920402ab704359c27423a3c78c7f150b2e
Andrew Hoefling <andrewh@fileonq.com>
2021-08-02 09:50:37 -0400

    Added new libraw build; Added test filts to test plan to only run libraw tests if there is a change to libraw

--- tests/FileOnQ.Imaging.Raw.Tests/Constants.cs
+++ tests/FileOnQ.Imaging.Raw.Tests/Constants.cs
@@ -7,6 +7,8 @@
 			// Defines test that complete full End to End testing
 			public const string Integration = "Integration";
 
+			public const string LibRaw = "LibRaw";
+
 			// Defines test that validates a specific function or unit of work.
 			public const string Unit = "Unit";
 		}
