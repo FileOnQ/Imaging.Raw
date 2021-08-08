2721cf9985428b0ea040fadb09c7e590679ea785
Andrew Hoefling <andrewh@fileonq.com>
2021-08-08 15:13:47 -0400

    Added thumbnail-resize options for benchmarking that tests various custom processors

--- benchmarks/tools/ThumbnailResize.cs
+++ benchmarks/tools/ThumbnailResize.cs
@@ -5,13 +5,16 @@ using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Diagnostics.Windows.Configs;
 using BenchmarkDotNet.Jobs;
 using FileOnQ.Imaging.Raw.Processors;
+using ImageMagick;
+using PhotoSauce.MagicScaler;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using SixLabors.ImageSharp.Processing.Processors.Transforms;
 
 namespace FileOnQ.Imaging.Raw.Benchmarking
 {
-	[SimpleJob(RuntimeMoniker.Net50, launchCount: 10, invocationCount: 10)]
+	[SimpleJob(RuntimeMoniker.Net50, launchCount: 5, invocationCount: 10)]
 	//[NativeMemoryProfiler]
 	//[MemoryDiagnoser]
 	[JsonExporterAttribute.Full]
@@ -29,21 +32,21 @@ namespace FileOnQ.Imaging.Raw.Benchmarking
 			output = Path.Combine(assemblyDirectory, "output.jpeg");
 		}
 
-		//[Benchmark(Baseline = true)]
-		//public void ExtractThumbnail()
-		//{
-		//	using (var image = new RawImage(filePath))
-		//	using (var thumb = image.UnpackThumbnail())
-		//	{
-		//		thumb.Process(new ThumbnailProcessor());
-		//		thumb.Write(output);
-		//	}
+		[Benchmark(Baseline = true)]
+		public void ExtractThumbnail()
+		{
+			using (var image = new RawImage(filePath))
+			using (var thumb = image.UnpackThumbnail())
+			{
+				thumb.Process(new ThumbnailProcessor());
+				thumb.Write(output);
+			}
 
-		//	File.Delete(output);
-		//}
+			File.Delete(output);
+		}
 
 		[Benchmark(Baseline = true)]
-		public void ExtractThumbnailAndResize_Bicubic()
+		public void ImageSharp_ThumbnailAndResize_Bicubic()
 		{
 			using (var image = new RawImage(filePath))
 			using (var thumb = image.UnpackThumbnail())
@@ -56,7 +59,7 @@ namespace FileOnQ.Imaging.Raw.Benchmarking
 		}
 
 		[Benchmark]
-		public void ExtractThumbnailAndResize_Box()
+		public void ImageSharp_ThumbnailAndResize_Box()
 		{
 			using (var image = new RawImage(filePath))
 			using (var thumb = image.UnpackThumbnail())
@@ -69,7 +72,7 @@ namespace FileOnQ.Imaging.Raw.Benchmarking
 		}
 
 		[Benchmark]
-		public void ExtractThumbnailAndResize_CatmullRom()
+		public void ImageSharp_ThumbnailAndResize_CatmullRom()
 		{
 			using (var image = new RawImage(filePath))
 			using (var thumb = image.UnpackThumbnail())
@@ -82,7 +85,7 @@ namespace FileOnQ.Imaging.Raw.Benchmarking
 		}
 
 		[Benchmark]
-		public void ExtractThumbnailAndResize_Hermite()
+		public void ImageSharp_ThumbnailAndResize_Hermite()
 		{
 			using (var image = new RawImage(filePath))
 			using (var thumb = image.UnpackThumbnail())
@@ -95,7 +98,7 @@ namespace FileOnQ.Imaging.Raw.Benchmarking
 		}
 
 		[Benchmark]
-		public void ExtractThumbnailAndResize_Lanczos2()
+		public void ImageSharp_ThumbnailAndResize_Lanczos2()
 		{
 			using (var image = new RawImage(filePath))
 			using (var thumb = image.UnpackThumbnail())
@@ -108,7 +111,7 @@ namespace FileOnQ.Imaging.Raw.Benchmarking
 		}
 
 		[Benchmark]
-		public void ExtractThumbnailAndResize_Lanczos3()
+		public void ImageSharp_ThumbnailAndResize_Lanczos3()
 		{
 			using (var image = new RawImage(filePath))
 			using (var thumb = image.UnpackThumbnail())
@@ -121,7 +124,7 @@ namespace FileOnQ.Imaging.Raw.Benchmarking
 		}
 
 		[Benchmark]
-		public void ExtractThumbnailAndResize_Lanczos5()
+		public void ImageSharp_ThumbnailAndResize_Lanczos5()
 		{
 			using (var image = new RawImage(filePath))
 			using (var thumb = image.UnpackThumbnail())
@@ -134,7 +137,7 @@ namespace FileOnQ.Imaging.Raw.Benchmarking
 		}
 
 		[Benchmark]
-		public void ExtractThumbnailAndResize_Lanczos8()
+		public void ImageSharp_ThumbnailAndResize_Lanczos8()
 		{
 			using (var image = new RawImage(filePath))
 			using (var thumb = image.UnpackThumbnail())
@@ -147,7 +150,7 @@ namespace FileOnQ.Imaging.Raw.Benchmarking
 		}
 
 		[Benchmark]
-		public void ExtractThumbnailAndResize_MitchellNetravali()
+		public void ImageSharp_ThumbnailAndResize_MitchellNetravali()
 		{
 			using (var image = new RawImage(filePath))
 			using (var thumb = image.UnpackThumbnail())
@@ -160,7 +163,7 @@ namespace FileOnQ.Imaging.Raw.Benchmarking
 		}
 
 		[Benchmark]
-		public void ExtractThumbnailAndResize_NearestNeighbor()
+		public void ImageSharp_ThumbnailAndResize_NearestNeighbor()
 		{
 			using (var image = new RawImage(filePath))
 			using (var thumb = image.UnpackThumbnail())
@@ -173,7 +176,7 @@ namespace FileOnQ.Imaging.Raw.Benchmarking
 		}
 
 		[Benchmark]
-		public void ExtractThumbnailAndResize_Robidoux()
+		public void ImageSharp_ThumbnailAndResize_Robidoux()
 		{
 			using (var image = new RawImage(filePath))
 			using (var thumb = image.UnpackThumbnail())
@@ -186,7 +189,7 @@ namespace FileOnQ.Imaging.Raw.Benchmarking
 		}
 
 		[Benchmark]
-		public void ExtractThumbnailAndResize_RobidouxSharp()
+		public void ImageSharp_ThumbnailAndResize_RobidouxSharp()
 		{
 			using (var image = new RawImage(filePath))
 			using (var thumb = image.UnpackThumbnail())
@@ -199,7 +202,7 @@ namespace FileOnQ.Imaging.Raw.Benchmarking
 		}
 
 		[Benchmark]
-		public void ExtractThumbnailAndResize_Spline()
+		public void ImageSharp_ThumbnailAndResize_Spline()
 		{
 			using (var image = new RawImage(filePath))
 			using (var thumb = image.UnpackThumbnail())
@@ -212,7 +215,7 @@ namespace FileOnQ.Imaging.Raw.Benchmarking
 		}
 
 		[Benchmark]
-		public void ExtractThumbnailAndResize_Triangle()
+		public void ImageSharp_ThumbnailAndResize_Triangle()
 		{
 			using (var image = new RawImage(filePath))
 			using (var thumb = image.UnpackThumbnail())
@@ -225,7 +228,7 @@ namespace FileOnQ.Imaging.Raw.Benchmarking
 		}
 
 		[Benchmark]
-		public void ExtractThumbnailAndResize_Welch()
+		public void ImageSharp_ThumbnailAndResize_Welch()
 		{
 			using (var image = new RawImage(filePath))
 			using (var thumb = image.UnpackThumbnail())
@@ -235,6 +238,52 @@ namespace FileOnQ.Imaging.Raw.Benchmarking
 			}
 
 			File.Delete(output);
+		}
+
+		[Benchmark]
+		public void MagicScaler_ThumbnailAndResize_Wic()
+		{
+			using (var image = new RawImage(filePath))
+			using (var thumb = image.UnpackThumbnail())
+			{
+				thumb.Process(new MagicScalerProcessor(200, 200));
+				thumb.Write(output);
+			}
+
+			File.Delete(output);
+		}
+
+		[Benchmark]
+		public void ImageMagick_ThumbnailAndResize()
+		{
+
+			using (var image = new RawImage(filePath))
+			using (var thumb = image.UnpackThumbnail())
+			{
+				thumb.Process(new ImageMagickResizerProcessor());
+				thumb.Write(output);
+			}
+
+			File.Delete(output);
+		}
+
+		class ImageMagickResizerProcessor : ThumbnailProcessor
+		{
+			byte[] buffer;
+			public override void Process(RawImageData data)
+			{
+				var image = new MagickImage(data.Buffer);
+				image.Resize(200, 200);
+
+				var stream = new MemoryStream();
+				image.Write(stream);
+				buffer = stream.ToArray();
+			}
+
+			public override void Write(RawImageData data, string file)
+			{
+				File.WriteAllBytes(file, buffer);
+			}
 		}
 
 		class ImageSharpResizeProcessor : ThumbnailProcessor
@@ -275,5 +324,77 @@ namespace FileOnQ.Imaging.Raw.Benchmarking
 				}
 			}
 		}
+
+		class MagicScalerProcessor : ThumbnailProcessor
+		{
+			byte[] buffer;
+			readonly int width, height;
+			public MagicScalerProcessor(int width, int height)
+			{
+				this.width = width;
+				this.height = height;
+			}
+
+			public override void Process(RawImageData data)
+			{
+				using (var stream = new MemoryStream())
+				{
+					var settings = new ProcessImageSettings
+					{
+						Width = this.width
+					};
+
+					MagicImageProcessor.ProcessImage(data.Buffer, stream, settings);
+					buffer = stream.ToArray();
+				}
+			}
+
+			public override void Write(RawImageData data, string file)
+			{
+				File.WriteAllBytes(file, buffer);
+			}
+		}
+
+		// I was thinking of testing out a raw resize algorithm
+		//class ResizeProcessor : ThumbnailProcessor
+		//{
+		//	byte[] buffer;
+		//	readonly int width, height;
+		//	public ResizeProcessor(int width, int height)
+		//	{
+		//		this.width = width;
+		//		this.height = height;
+		//	}
+
+		//	public override void Process(RawImageData data)
+		//	{
+		//		var info = new ImageMagick.MagickImageInfo(data.Buffer);
+
+		//		var start = DateTime.Now;
+		//		var i = Image.Load<Rgb24>(data.Buffer);
+
+		//		var end = DateTime.Now;
+		//		var elapsed = end.Subtract(start).TotalMilliseconds;
+
+		//		int dataSize = (width * height);// * data.Bits * data.Colors;
+		//		buffer = new byte[dataSize];
+		//		(double X, double Y) ratio = (0, 0);
+		//		ratio.X = info.Width / (double)width;
+		//		ratio.Y = info.Height / (double)height;
+
+		//		for (int y = 0; y < height; y++)
+		//		{
+		//			for (int x = 0; x < width; x++)
+		//			{
+		//				var pixelX = Math.Floor((double)x * ratio.X);
+		//				var pixelY = Math.Floor((double)y * ratio.Y);
+
+		//				int position = (y * width) + x;
+		//				int sourcePosition = (int)((pixelY * info.Width) + pixelX);
+		//				buffer[position] = data.Buffer[sourcePosition];
+		//			}
+		//		}
+		//	}
+		//}
 	}
 }
