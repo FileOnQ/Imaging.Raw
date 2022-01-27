0cda1a3b2459e53f7c2bfea03917d3bc891ac704
Brittany Fisher <bfisher@iowacomputergurus.com>
2022-01-27 11:05:13 -0600

    splitting batch commands

--- build/libraw.bat
+++ build/libraw.bat
@@ -1,18 +1,30 @@
-@echo off
+::@echo off
 title Building LibRaw
 set arch=%1
 set vcvar=
 
-for /f "usebackq tokens=1* delims=" %%x in (`"%ProgramFiles%\Microsoft Visual Studio\Installer\vswhere.exe" -find **\vcvarsall.bat`) do set vcvar="%%~x"
+set progFiles="%ProgramFiles%\Microsoft Visual Studio\Installer\vswhere.exe"
+for /f "usebackq tokens=1 delims=" %%x in (`"%progFiles%" -find **\vcvarsall.bat`) do set vcvar="%%~x"
 
-if defined ProgramFiles(x86) if not defined vcvar (
-	for /f "usebackq tokens=1* delims=" %%x in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -find **\vcvarsall.bat`) do set vcvar="%%~x"
-)
+if defined vcvar goto fileFound
 
-if not defined vcvar (	
-	echo "unable to find 'vcvarsall.bat'. visual studio c++ package needs to be installed"
-	exit /b 2
-)
+
+if not defined ProgramFiles(x86) goto fileNotFound
+
+
+for /f "usebackq tokens=1 delims=" %%y in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -find **\vcvarsall.bat`) do set vcvar="%%~y"
+if defined vcvar goto fileFound
+
+
+
+:fileNotFound
+echo "unable to find 'vcvarsall.bat'. visual studio c++ package needs to be installed"
+exit /b 2
+
+
+
+:fileFound
+echo "csvarsall.bat was found, continuing"
 
 call %vcvar% %arch%
 cd ../LibRaw
